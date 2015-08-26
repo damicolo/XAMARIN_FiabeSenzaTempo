8f212c5ea41503050036e67f697c5c437c70ba62
Damiano Colombo <damiano.colombo@satispay.com>
2015-08-26 12:45:52 +0200

    minor refactoring

--- FiabeSenzaTempo/MainActivity.cs
+++ FiabeSenzaTempo/MainActivity.cs
@@ -17,49 +17,47 @@ namespace FiabeSenzaTempo
 	[Activity (Label = "Fiabe Senza Tempo", MainLauncher = true, Icon = "@drawable/icon", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
 	public class MainActivity : Activity
 	{
-		public class videoItem{
-			public string Title;
-			public string URL;
-		}
-
-		ListView myList;
-		ArrayAdapter adapter;
-		string theFileList = "";
-		List<FiabeSenzaTempo.MainActivity.videoItem> theVideos = new List<FiabeSenzaTempo.MainActivity.videoItem>();
+		private const string m_playlist = "http://damicolo1.azurewebsites.net/FavoleSenzaTempoYoutube.txt";
+		ListView m_myList;
+		ArrayAdapter m_adapter;
+		List<videoItem> m_theVideos = new List<videoItem>();
 
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
-
 			SetContentView (Resource.Layout.Main);
 
+			// prevent screen lock; require WAKE_LOCK permission in the manifest
 			this.Window.SetFlags(WindowManagerFlags.KeepScreenOn, WindowManagerFlags.KeepScreenOn);
 
+			// retrieve the playlist from azure
 			WebClient httpclient = new WebClient (); 
-			theFileList = httpclient.DownloadString ("http://damicolo1.azurewebsites.net/FavoleSenzaTempoYoutube.txt");
+			string theFileList = httpclient.DownloadString (m_playlist);
 			theFileList = theFileList.Replace ("\r\n", ",");
 			string[] VideoList = theFileList.Split (new char[] { ','});
 			List<string> myData = new List<string> ();
 			foreach (string s in VideoList) {
 				string[] elements = s.Split (new char[]{ ';' });
-				theVideos.Add (new videoItem (){ Title = elements [1], URL = elements [2] });
-				myData.Add (theVideos[theVideos.Count-1].Title);
+				m_theVideos.Add (new videoItem (){ Title = elements [1], URL = elements [2] });
+				myData.Add (m_theVideos[m_theVideos.Count-1].Title);
 			}
-			myList = (ListView)this.FindViewById (Resource.Id.myListView);
-			myList.ItemClick += myList_ItemClick;
-			adapter = new ArrayAdapter (this, Android.Resource.Layout.SimpleListItem1,myData);
-			myList.Adapter = adapter;
 
+			// set the lis and adapter
+			m_myList = (ListView)this.FindViewById (Resource.Id.myListView);
+			m_myList.ItemClick += myList_ItemClick;
+			m_adapter = new ArrayAdapter (this, Android.Resource.Layout.SimpleListItem1,myData);
+			m_myList.Adapter = m_adapter;
+
+			// advertising setup
 			AdView mAdView = (AdView) this.FindViewById(Resource.Id.adView);
 			AdRequest adRequest = new AdRequest.Builder ().Build ();
 			mAdView.LoadAd(adRequest);
-
 		}
 			
 		private async void myList_ItemClick (object sender, AdapterView.ItemClickEventArgs e)
 		{
-			videoItem sellectedItem = theVideos [e.Position];
+			videoItem sellectedItem = m_theVideos [e.Position];
 			string videoID = sellectedItem.URL.Split (new char[] { '=' })[1];
 			try
 			{
---
+++ FiabeSenzaTempo/videoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FiabeSenzaTempo
+{
+	public class videoItem
+	{
+		public string Title;
+		public string URL;
+
+		public videoItem ()
+		{
+		}
+	}
+}
+
